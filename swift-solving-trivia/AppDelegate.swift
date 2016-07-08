7dd96781b972169852e94b094dbcea0c9160620b
joelconnects <joelconnects@gmail.com>
2016-07-08 14:25:06 -0400

    removed solution from AppDelegate

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -17,27 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      write your function here
      
      */
-    func solveTrivia(statesAndCapitals : [String: String]) -> String {
-        
-        var isMatch = false
-        
-        for (state, capital) in statesAndCapitals {
-            
-            let stateArray = Set(state.lowercaseString.characters).sort()
-            let capitalArray = Set(capital.lowercaseString.characters).sort()
-            isMatch = (stateArray.map { capitalArray.contains($0) }).contains(true)
-            
-            if !isMatch {
-                
-                return state
-            }
-        
-        }
-        
-        return "No answer found"
-        
-    }
-
 
 }
 
